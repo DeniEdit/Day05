0f955ca8fbef09741115fb2c07e3ed74a9282513
DeniEdit <milicevic.deni@gmail.com>
2021-03-12 15:25:56 +0100

    Exercise 10

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    //Only change code below this line
+
+    var myString = "Jello World";
+    myString = "Hello World";
+
+    
+
+    //Only change code below this line
+
+    return myString;
+    
+}
+
+console.log(main());
+module.exports = main;
