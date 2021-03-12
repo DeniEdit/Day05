9ed01b86cbe6ad86a7fbf53828c7687d312f4149
DeniEdit <milicevic.deni@gmail.com>
2021-03-12 14:52:05 +0100

    Exercise 05

---
+++ ex05/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    //Only change code below this line
+
+    var myString = "Concatenation is the process of appendin";
+    myString += "one string to the end of another string.";
+
+    //Only change code below this line
+
+    return myString;
+    
+}
+
+console.log(main());
+module.exports = main;
