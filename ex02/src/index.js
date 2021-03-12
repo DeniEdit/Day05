21e4355113c43bbe57b29625103c2ce7284d35b4
DeniEdit <milicevic.deni@gmail.com>
2021-03-12 14:15:57 +0100

    Exercise 02

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    //Only change code below this line
+
+    var myLink = '<a href=\"http://wwww.example.com\" target=\"_blank\">Link</a>';
+   
+    //Only change code below this line
+
+    return myLink;
+    
+}
+
+console.log(main());
+module.exports = main;
